abe4950749d4e3ce37bf7bf1e5935e6f3750464e
yash <write2yashreddy@gmail.com>
2019-03-29 07:19:58 +0530

    Added the web element scrapers to access the simulated results and output them. Also commented out are the libraries necessary to store the results in an excel file, which will be the next part of this project.

--- auto_simulate.py
+++ auto_simulate.py
@@ -6,7 +6,10 @@ from selenium.common.exceptions import ElementNotInteractableException
 
 import time  
    
-from selenium.webdriver.common.keys import Keys  
+from selenium.webdriver.common.keys import Keys
+
+import xlwt 
+from xlwt import Workbook   
 
 #taking email and password as inputs
 email=input("enter email id :")
@@ -14,6 +17,11 @@ password=input("enter password :")
 
 # Creating an instance webdriver 
 browser = webdriver.Firefox()  
+
+#initialise the excel workbook and sheet
+#wb = Workbook()
+#sheet1 = wb.add_sheet('Sheet 1')
+
 browser.get('https://websim.worldquantvrc.com/simulate')
 flag=True
 while(flag):
@@ -46,17 +54,49 @@ while(flag):
 #model_selection here
 
 ##################################################################################
+#simulation		
 		browser.find_element_by_css_selector('.inputarea').send_keys("rank(sales/assets)")
 		time.sleep(1)
 		browser.find_element_by_xpath('//button[contains(@class,"intro-step-5")]').click()
 		flag=False
 	except NoSuchElementException:
 		time.sleep(2.0)	
+##################################################################################
+#generate self and prod correlation
 flag=True
 time.sleep(10)
 while(flag):
 	try:
-		summary=browser.find_element_by_xpath('//div[contains(@class,"summary__status-button")]').text
+		self_prod_correlation_buttons=browser.find_elements_by_xpath('//div[contains(@class,"content-status-time-refresh")]')
+		self_prod_correlation_buttons[0].click()
+		self_prod_correlation_buttons[1].click()
 		flag=False
 	except NoSuchElementException:
 		time.sleep(2.0)
+##################################################################################
+#finding and storing the results in an excel sheet
+
+flag=True
+while(flag):
+	try:
+		status=browser.find_element_by_xpath('//div[contains(@class,"summary__status-button")]').text
+		print(status)
+		title_grabbers=browser.find_elements_by_xpath('//div[contains(@class,"summary-metrics-info--title")]')
+		value_grabbers=browser.find_elements_by_xpath('//div[contains(@class,"summary-metrics-info--data")]')
+		for i in range(0,5):
+			print(title_grabbers[i].text+" - "+value_grabbers[i].text)
+		flag=False
+	except NoSuchElementException:
+		time.sleep(2.0)
+
+flag=True
+while(flag):
+	try:
+		self_prod_correlations=browser.find_elements_by_xpath('//div[contains(@class,"correlation__content-status-higher-value")]')
+		if(self_prod_correlations[0].text=="-" or self_prod_correlations[1].text=="-"):
+			continue
+		print("self correlation - "+self_prod_correlations[0].text)
+		print("prod correlation - "+self_prod_correlations[1].text)
+		flag=False
+	except NoSuchElementException:
+		time.sleep(2.0)		
